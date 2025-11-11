8bf3a5b0c623cca0f842ac0a487064ab352ffb3b
Abdulrahman <abd3lra7man.078@gmail.com>
2025-11-12 01:29:28 +0300

    create migration warehouse entities.

---
+++ HappyWarehouse.Infrastructure/Migrations/20251111222851_create-warehouse-entities.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyWarehouse.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class createwarehouseentities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CountryName = table.Column<string>(type: "TEXT", maxLength: 60, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    DeletedBy = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    RestoredAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    RestoredBy = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CountryId", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Warehouses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    WarehouseName = table.Column<string>(type: "TEXT", maxLength: 60, nullable: false),
+                    AddressName = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    CityName = table.Column<string>(type: "TEXT", maxLength: 60, nullable: false),
+                    CountryId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedByUserId = table.Column<int>(type: "INTEGER", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "TEXT", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    DeletedBy = table.Column<string>(type: "TEXT", nullable: true),
+                    RestoredAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    RestoredBy = table.Column<string>(type: "TEXT", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WarehouseId", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Warehouse_Country",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Warehouses_Users_CreatedByUserId",
+                        column: x => x.CreatedByUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WarehouseItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ItemName = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false),
+                    SkuCode = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    Qty = table.Column<int>(type: "INTEGER", nullable: false),
+                    CostPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MsrpPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    WarehouseId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedByUserId = table.Column<int>(type: "INTEGER", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    DeletedBy = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    RestoredAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    RestoredBy = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WarehouseItemId", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WarehouseItems_Users_CreatedByUserId",
+                        column: x => x.CreatedByUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_WarehouseItems_Warehouses_WarehouseId",
+                        column: x => x.WarehouseId,
+                        principalTable: "Warehouses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_CountryName",
+                table: "Countries",
+                column: "CountryName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WarehouseItems_CreatedByUserId",
+                table: "WarehouseItems",
+                column: "CreatedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WarehouseItems_ItemName",
+                table: "WarehouseItems",
+                column: "ItemName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WarehouseItems_WarehouseId",
+                table: "WarehouseItems",
+                column: "WarehouseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Warehouses_CountryId",
+                table: "Warehouses",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Warehouses_CreatedByUserId",
+                table: "Warehouses",
+                column: "CreatedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Warehouses_WarehouseName",
+                table: "Warehouses",
+                column: "WarehouseName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WarehouseItems");
+
+            migrationBuilder.DropTable(
+                name: "Warehouses");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
