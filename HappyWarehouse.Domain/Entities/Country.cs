cdefaa02b945bf3ba9a99dd15e7112511a7f09cd
Abdulrahman <abd3lra7man.078@gmail.com>
2025-11-12 01:07:10 +0300

    Create methods in domain country: create, update, soft delete, restore.

--- HappyWarehouse.Domain/Entities/Country.cs
+++ HappyWarehouse.Domain/Entities/Country.cs
@@ -16,4 +16,59 @@ public class Country: Entity<int>
     /// Navigation Property, One country can have many warehouses. 
     /// </summary>
     public ICollection<Warehouse> Warehouses = [];
+
+    private Country() {}
+
+    public Country(string name)
+    {
+        Name = name;
+    }
+    
+    #region Create Country.
+    /// <summary> Method to Create Country. </summary>
+    public Country Create(string countryName, string? createdBy = null)
+    {
+        if (string.IsNullOrWhiteSpace(countryName))
+        {
+            throw new ArgumentNullException(nameof(countryName));
+        }
+        
+        var country = new Country(countryName);
+
+        MarkCreated(createdBy);
+        
+        return country;
+    }
+    #endregion
+
+    #region Update Country.
+    /// <summary> Method to Update Country. </summary>
+    public void Update(string countryName, string? modifiedBy = null)
+    {
+        if (string.IsNullOrWhiteSpace(countryName))
+        {
+            throw new ArgumentNullException(nameof(countryName));
+        }
+        
+        Name = countryName;
+        
+        MarkModified(modifiedBy);
+    }
+    #endregion
+
+    #region Softly Delete Country.
+    /// <summary> Method to Softly Delete Country. </summary>
+    public void SoftDelete(string? deletedBy = null)
+    {
+        MarkDeleted(deletedBy);
+    }
+    #endregion
+
+    #region Restore Deleted Country.
+    /// <summary> Method to Restore Deleted Country. </summary>
+    public void Restore(string? restoredBy = null)
+    {
+        MarkRestored(restoredBy);
+    }
+    #endregion
 }
