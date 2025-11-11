95a53020487fbcfe38c20f02ce6312a504a637d7
Abdulrahman <abd3lra7man.078@gmail.com>
2025-11-11 14:32:19 +0300

    Create ApplicationUser, ApplicationRole entities.

---
+++ HappyWarehouse.Domain/IdentityEntities/ApplicationRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HappyWarehouse.Domain.IdentityEntities;
+
+public class ApplicationRole: IdentityRole<int>
+{
+    
+}
---
+++ HappyWarehouse.Domain/IdentityEntities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HappyWarehouse.Domain.IdentityEntities;
+
+public class ApplicationUser: IdentityUser<int>
+{
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiration { get; set; } 
+    
+    public bool IsDeleted { get; set; } = false;
+}
